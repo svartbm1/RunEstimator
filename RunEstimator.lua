b2919d1575a14cc786ba432730be59afc35303d5
svartbm1 <minna.svartback@aalto.fi>
2016-01-17 22:26:12 +0200

    Fix code to working
    Fix error in parameters of the recursive function.

--- RunEstimator.lua
+++ RunEstimator.lua
@@ -8,15 +8,18 @@ counter = function(dropRate, desired)
 	kills = kills + 1
 	soFar = soFar + (100-soFar) * (dropRate/100)
 	if soFar < desired then
-		return counter(result)		
+		return counter(dropRate, desired)		
 	else 	    
 		return kills
 	end
 
  end
+
  
  print( "2The return is: " .. counter(50, 73) )
- print( "1The return is:  .. counter(100, 45) ")
- print( "7The return is:  .. counter(10, 50) ")
- print( "7The return is:  .. counter(30, 90) ")
+ kills = 0
+ soFar = 0
+ print( "1The return is: "  .. counter(100, 45) )
+ print( "7The return is: "  .. counter(10, 50) )
+ print( "7The return is: "  .. counter(30, 90) )
  
