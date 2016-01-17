7afb7f59143ee0545de2ba0d63189c108cd22a00
svartbm1 <minna.svartback@aalto.fi>
2016-01-17 22:22:22 +0200

    Import directory
    Add work to github

---
+++ RunEstimator.lua
@@ -0,0 +1,22 @@
+print("Hello " .. UnitName("player") .. ", you are so awesome...");
+
+local counter
+local kills = 0
+local soFar = 0
+
+counter = function(dropRate, desired)
+	kills = kills + 1
+	soFar = soFar + (100-soFar) * (dropRate/100)
+	if soFar < desired then
+		return counter(result)		
+	else 	    
+		return kills
+	end
+
+ end
+ 
+ print( "2The return is: " .. counter(50, 73) )
+ print( "1The return is:  .. counter(100, 45) ")
+ print( "7The return is:  .. counter(10, 50) ")
+ print( "7The return is:  .. counter(30, 90) ")
+ 
