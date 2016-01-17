2aa105253cdaa3d23e5cea49951d24c0eb493e7a
svartbm1 <minna.svartback@aalto.fi>
2016-01-18 00:42:26 +0200

    Get user input
    User input now works

--- RunEstimator.lua
+++ RunEstimator.lua
@@ -1,7 +1,5 @@
-print("Hello " .. UnitName("player") .. ", you are so awesome...");
+print("Run estimator is enabled.");
 	
--- self:RegisterChatCommand("runs", "OnChatCommand")
-
 local counter
 local kills = 0
 local soFar = 0
@@ -18,7 +16,7 @@ end
 
 
  
-print( "2The return is: " .. counter(50, 73) )
+print( "2The return is: " .. counter(50.00, 73) )
 	kills = 0
     soFar = 0
 print( "1The return is: "  .. counter(100, 45) )
@@ -30,13 +28,39 @@ print( "7The return is: "  .. counter(10, 50) )
 print( "7The return is: "  .. counter(30, 90) )
  
  
-local line = "33 90"
-local list = {}
-for i in string.gmatch(line, "[^%s]+") do  
-   --if 0 < i and i < 100 then 
-   table.insert(list, i)
-   print("'"..i.."'")
-end
 
-print(list[1].." and "..list[2])
- print(string.format("%2.2f", list[1])/11)
+--for i in string.gmatch(line, "[^%s]+") do  
+   --if 0 < i and i < 100 then 
+  -- table.insert(list, i)
+  -- print("'"..i.."'")
+--end
+
+--print(list[1].." and "..list[2])
+ ---print(string.format("%2.2f", list[1])/11)
+ 
+ local Estimator = function(input)
+	local line = "33 90"
+	local list = {}
+	for i in string.gmatch(input, "[^%s]+") do  
+		table.insert(list, i)
+		print(i.." added")
+	end
+	print("'"..string.format("%2.2f", list[1]).."'")
+	print("'"..string.format("%2.2f", list[2]).."'")
+	local input1 = tonumber(string.format("%2.2f", list[1]))
+	local input2 = tonumber(string.format("%2.2f", list[2]))
+	kills = 0
+    soFar = 0
+	print( "7The return is: "  .. counter(30.00, 90.00))
+	kills = 0
+    soFar = 0
+	print("RESULT: " .. counter(input1, input2))
+	
+ 
+ end
+ 
+ SLASH_RUNESTIMATOR1 = "/runs"
+ SLASH_RUNESTIMATOR2 = "/runestimator"
+SlashCmdList["RUNESTIMATOR"] = function(msg)
+	Estimator(msg)
+end
